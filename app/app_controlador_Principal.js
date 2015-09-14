a23b0a3b72b7cb25aeedfe7227e866e94bf4fb3d
UnJavaScripter <eltu.khan@gmail.com>
2015-09-13 19:50:11 -0500

    Agregamos lista de mensajes, formulario de envío y campo para nombre de usuario

--- app/app_controlador_Principal.js
+++ app/app_controlador_Principal.js
@@ -11,6 +11,25 @@
 		var ref = new Firebase("https://angularfire-gdg.firebaseio.com/");
 
 		vm.mensajes = $firebaseArray(ref);
+
+
+		// Creamos un nombre aleatorio para vistante que se usará si el usuario no pone un nombre
+		var usuarioVisitante = "Visitante_" + Math.ceil(Math.random()*1000);
+
+
+		vm.enviar = function(){
+
+			vm.mensajes.$add({
+
+				remitente: vm.miNombre || usuarioVisitante,
+				texto: vm.mensajeNuevo
+				
+			});
+
+			// Borramos el campo de texto y hacemos 'focus' sobre él
+			vm.mensajeNuevo = "";
+			document.getElementById('mensajeNuevo').focus();
+		};
 	}
 
 })();
